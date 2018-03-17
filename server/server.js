dbb39fb12b94faa246f73e22324cf4d28fe22d20
Sam Standing <samzstanding@gmail.com>
2018-03-17 09:52:02 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');
+const corsIfInDevelopment = require('./modules/cors-if-on-development');
+const sessionMiddleware = require('./modules/session-middleware');
+
+// start up the mongo database
+require('./modules/database');
+
+const passport = require('./strategies/user.strategy');
+
+// Route includes
+const userRouter = require('./routes/user.router');
+
+// allows cross-origin requests from localhost:3000 when on development
+app.use(corsIfInDevelopment);
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Routes */
+app.use('/api/user', userRouter);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+const CONSTANTS = {
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || '/api',
+};
+
+export default CONSTANTS;
